22236752111a6889960758b14e4100c6df86cd84
David Leonardo <dlpuertaj@unal.edu.co>
2018-09-02 13:31:45 -0500

    log4j

--- spring_jdbc/src/main/java/org/sistema/spring/log4j/Main.java
+++ spring_jdbc/src/main/java/org/sistema/spring/log4j/Main.java
@@ -1,6 +1,7 @@
 package org.sistema.spring.log4j;
 
-import org.apache.logging.log4j.Logger;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.sistema.spring.log4j.models.GreetingEngine;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -9,13 +10,21 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 		
 		GreetingEngine greetingEngine = (GreetingEngine) context.getBean("greetingEngine");
 		
 		 Logger logger = Logger.getLogger(Main.class);
 		 
-		 logger.set
+		 logger.setLevel(Level.ALL);
+		 logger.trace("Simple trace");
+		 logger.debug("Debug message");
+		 logger.error("Error message");
+		 logger.fatal("Fatal error");
+		 
+		 System.out.println(greetingEngine.greetMe());
+		 
+		 //TODO: Create log4j.properties file
 	}
 
 }
